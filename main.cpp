fcb26749ac6b25a97b366a3de75a9d7588e61038
ZZY <909645105@qq.com>
2015-04-06 23:09:37 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 //main
 #include "my_re.h"
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	NFA a = NFA("\\((fuck)*|(shit)*");
+	NFA a = NFA("(fuck)*|(shit)*");
+	a.DeleteEpsilon();
 	system("pause");
 	return 0;
 }
